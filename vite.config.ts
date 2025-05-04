bc9d4c5b615c89a6cde6cd8db3aa67f8b566a2fc
BomberFish <bomberfish@bomberfish.ca>
2025-05-04 14:58:01 -0400

    tweak vite config more

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,32 @@
-import { defineConfig } from 'vite';
-import { dreamlandPlugin } from 'vite-plugin-dreamland';
-import legacy from '@vitejs/plugin-legacy';
+import { defineConfig } from "vite";
+import { dreamlandPlugin } from "vite-plugin-dreamland";
+import legacy from "@vitejs/plugin-legacy";
 
 export default defineConfig({
-    plugins: [dreamlandPlugin(), legacy({
-      targets: ['partially supports flexbox', 'not dead', 'Blackberry 7', 'BlackBerry 10', 'Firefox ESR']
-    })],
+    plugins: [
+        dreamlandPlugin(),
+        legacy({
+            targets: [
+                "partially supports flexbox",
+                "supports proxy",
+                "not dead",
+                "Blackberry 7",
+                "BlackBerry 10",
+                "Firefox ESR",
+            ],
+        }),
+    ],
     base: "./",
     build: {
-      sourcemap: true,
-      cssMinify: "lightningcss",
-      minify: "terser",
+        sourcemap: true,
+        cssMinify: "lightningcss",
+        minify: "terser",
     },
     css: {
-      devSourcemap: true,
+        devSourcemap: true,
     },
     server: {
-      host: true,
-      cors: false,
+        host: true,
+        cors: false,
     },
 });
